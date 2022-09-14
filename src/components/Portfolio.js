8a47c4449acd7f5ed3d280e563073e2fdedc7015
Prawiro <yusufprawiro14@gmail.com>
2022-09-14 17:48:00 +0700

    add portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaPizzaSlice } from "react-icons/fa";
-import { SiPostgresql, SiMongodb } from "react-icons/si";
+import { MdSpaceDashboard } from "react-icons/md";
 
 const Portfolio = () => {
   return (
@@ -9,15 +9,42 @@ const Portfolio = () => {
         Project Portfolio
       </p>
       <div className="flex flex-wrap justify-center pt-2">
-        <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
-          <FaPizzaSlice
-            color="#FFC35A"
-            className="mx-auto text-2xl sm:text-4xl"
-          />
-          <p className="mt-6 text-xl sm:text-xl font-semibold text-center">
-            Order Pizza
-          </p>
-        </div>
+        <a
+          href="https://yprawirocode.github.io/order-pizza/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
+            <FaPizzaSlice
+              color="#FFC35A"
+              className="mx-auto text-2xl sm:text-4xl"
+            />
+            <p className="mt-6 text-xl sm:text-xl font-semibold text-center">
+              Order Pizza
+            </p>
+            <p className="text-xl sm:text-xl font-semibold text-center">
+              Click Me
+            </p>
+          </div>
+        </a>
+        <a
+          href="https://dashboard-yprawirocode.netlify.app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52">
+            <MdSpaceDashboard
+              color="#00FFFF"
+              className="mx-auto text-2xl sm:text-4xl"
+            />
+            <p className="mt-3 text-l sm:text-l font-semibold text-center">
+              Simple Dashboard
+            </p>
+            <p className="text-l sm:text-l font-semibold text-center">
+              Click Me
+            </p>
+          </div>
+        </a>
       </div>
     </div>
   );
